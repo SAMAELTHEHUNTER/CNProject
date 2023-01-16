92cb06139eeda4615eed40f1f35670f650b26972
rozhan <rozhan1381@yahoo.com>
2023-01-16 15:19:18 +0330

    input stream changes

--- src/Client.java
+++ src/Client.java
@@ -11,7 +11,8 @@ public class Client implements Runnable {
     String serverAddress = "127.0.0.1";
     ObjectInputStream reader;
     ObjectOutputStream writer;
-    Scanner scanner = new Scanner(System.in);
+    //Scanner scanner = new Scanner(System.in);
+    BufferedReader scanner = new BufferedReader(new InputStreamReader(System.in));
     public static final String GREEN = "\033[0;32m";
     public static final String RED = "\u001B[31m";
     public static final String RESET = "\033[0m";
@@ -24,8 +25,23 @@ public class Client implements Runnable {
         setUp.loadSettings();
         serverPort = setUp.getServerPort();
 
+        Thread thread= new Thread(() -> {
+           try {
+               String userInput;
+               while ((userInput = scanner.readLine()) != null) {
+                   writer.writeUTF(userInput);
+                   writer.flush();
+                   // System.out.println("echo: " + in.readLine());
+               }
+           } catch (IOException e) {
+               e.printStackTrace();
+           }
+        });
 
-        try {
+            thread.start();
+
+
+            try {
             mSocket = new Socket(serverAddress, serverPort);
             System.out.println("connected to server ....");
             writer = new ObjectOutputStream(mSocket.getOutputStream());
@@ -36,7 +52,7 @@ public class Client implements Runnable {
 
             //Thread thread = new Thread(reader);
 
-            writer.writeUTF(scanner.nextLine());
+            writer.writeUTF(scanner.readLine());
             writer.flush();
 //
 //            msg = reader.readUTF();
@@ -86,32 +102,12 @@ public class Client implements Runnable {
                 System.out.println(e);
             }
 
-//            try {
-//                sendAnswer sa = new sendAnswer();
-//                sa.start();
-//                sa.join();
-//                String out = sa.myStop();
-//                writer.writeUTF(out);
-//                writer.flush();
-//            } catch (IOException | InterruptedException e) {
-//                e.printStackTrace();
-//            }
-
-//            Thread thread= new Thread(() -> {
-//                Scanner scanner = new Scanner(System.in);
-//                String answer = scanner.nextLine();
-//                if (answer.startsWith("start chat with")){
-//
-//                }
-//            });
-//
-//            thread.start();
 
         }
 
     }
 
-    public void showMessage(String text){
+    public void showMessage(String text) throws IOException {
         String[] str = text.split("/");
 
         switch (str[0]) {
@@ -169,6 +165,7 @@ public class Client implements Runnable {
                 break;
 
             case "Question":
+                scanner = new BufferedReader(new InputStreamReader(System.in));
                 answered = false;
                 String question = str[1];
                 String[] options = str[2].split(",");
@@ -180,71 +177,73 @@ public class Client implements Runnable {
                 }
 
                 System.out.println(RED + "ATTENTION : If you don't have an answer for the given question, please press Enter.\n" + RESET);
-                while (wasAMessage && !answered){
-                    try {
-                        class sendAnswer extends Thread {
-                            private String answer;
-                            private String tmp;
 
-                            public sendAnswer() {
-                                tmp = "-1";
-                            }
-
-                            @Override
-                            public void run() {
-                                Scanner sc = new Scanner(System.in);
-                                tmp = sc.nextLine();
-//                            System.out.println(tmp);
-                            }
-
-                            public String myStop() {
-                                //answer = Integer.toString(tmp);
-                                answer = tmp;
-//                            super.stop();
-                                return answer;
-                            }
-                        }
-                        sendAnswer sa = new sendAnswer();
-                        sa.start();
-                        sa.join();
-                        String out = sa.myStop();
-                        writer.writeUTF(out);
-                        writer.flush();
-
-                        if (out.contains("message to")){
-                            wasAMessage = true;
-                        }
-                        else{
-                            answered = true;
-                            wasAMessage = false;
-                        }
+//                String userInput;
+//                while ((userInput = scanner.readLine()) != null) {
+//                    writer.writeUTF(userInput);
+//                    writer.flush();
+//                   // System.out.println("echo: " + in.readLine());
+//                }
 
 
-
-
-//                    System.out.println("jalil: " + out);
-//                    if (out.equals("-1")) {
-//                        System.out.println("please enter: ");
+//                while (wasAMessage && !answered){
+//                    try {
+//                        class sendAnswer extends Thread {
+//                            private String answer;
+//                            private String tmp;
+//
+//                            public sendAnswer() {
+//                                tmp = "-1";
+//                            }
+//
+//                            @Override
+//                            public void run() {
+//                                Scanner sc = new Scanner(System.in);
+//                                tmp = sc.nextLine();
+////                            System.out.println(tmp);
+//                            }
+//
+//                            public String myStop() {
+//                                //answer = Integer.toString(tmp);
+//                                answer = tmp;
+////                            super.stop();
+//                                return answer;
+//                            }
+//                        }
+//                        sendAnswer sa = new sendAnswer();
+//                        sa.start();
+//                        sa.join();
+//                        String out = sa.myStop();
+//                        writer.writeUTF(out);
+//                        writer.flush();
+//
+//                        if (out.contains("message to")){
+//                            wasAMessage = true;
+//                        }
+//                        else{
+//                            answered = true;
+//                            wasAMessage = false;
+//                        }
+//
+//
+//
+//
+////                    System.out.println("jalil: " + out);
+////                    if (out.equals("-1")) {
+////                        System.out.println("please enter: ");
+////                    }
+////                    System.out.println("jalil: " + out);
+//
+//
+//                    } catch (IOException | InterruptedException e) {
+//                        e.printStackTrace();
 //                    }
-//                    System.out.println("jalil: " + out);
-
-
-                    } catch (IOException | InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-
-                //doest work if the client decides not to answer a question because scanner waits for input
+//                }
 
                 break;
 
             default:
                 System.out.println(text);
-
-//            case "GlobalMsg":
-//                showGlobalMsg(str[1],str[2]);
-//                break;
-            //input.useDelimiter("~~end~~");
 
         }
     }
